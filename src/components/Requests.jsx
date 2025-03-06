4c8d30468a684ca79892693305b63b2b5e297612
bamidelemo <bamidelemo@gmail.com>
2025-03-06 06:10:20 +0100

    .

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -5,7 +5,7 @@ function Requests() {
   return (
     <>
       <div class="mt-12 sm:mt-24">
-        <div class="flex justify-between mx-4 mb-2">
+        <div class="flex justify-between mx-4 mb-1">
           <h2 class="text-lg sm:text-xl font-semibold">
             👋 Students Requests:
           </h2>
@@ -27,21 +27,60 @@ function Requests() {
           </div>
         </div>
         <div class="mx-4 space-y-4">
-          <div class="border border-gray-300 bg-gray-50 p-3 rounded-lg hover:bg-purple-100 cursor-pointer">
-            <span class="text-gray-600">2 days ago by 200 level student:</span>
-            <br />I am looking for 1 room self contained around the main gate.
+          <div class="space-y-2 border border-gray-300 bg-gray-50 p-3 rounded-lg hover:bg-purple-100 cursor-pointer">
+            <div class="flex space-x-2 text-xs text-gray-600 lowercase">
+              <span class="bg-gray-200 border border-gray-300 py-0.5 px-1 rounded-full">
+                2 Days Ago
+              </span>
+              <span class="bg-gray-200 border border-gray-300 py-0.5 px-1 rounded-full">
+                200 level student
+              </span>
+            </div>
+            <div>
+              I am looking for 1 room self contained around the main gate.
+            </div>
           </div>
-          <div class="border border-gray-300 bg-gray-50 p-3 rounded-lg hover:bg-purple-100 cursor-pointer">
-            <span class="text-gray-600">23 days ago by 400 level student:</span>
-            <br />
-            In need of room anywhere around the school back gate please. It is
-            very urgent.
+          <div class="space-y-2 border border-gray-300 bg-gray-50 p-3 rounded-lg hover:bg-purple-100 cursor-pointer">
+            <div class="flex space-x-2 text-xs text-gray-600 lowercase">
+              <span class="bg-gray-200 border border-gray-300 py-0.5 px-1 rounded-full">
+                23 Days Ago
+              </span>
+              <span class="bg-gray-200 border border-gray-300 py-0.5 px-1 rounded-full">
+                400 level student
+              </span>
+            </div>
+            <div>
+              In need of room anywhere around the school back gate please. It is
+              very urgent.
+            </div>
           </div>
-          <div class="border border-gray-300 bg-gray-50 p-3 rounded-lg hover:bg-purple-100 cursor-pointer">
-            <span class="text-gray-600">29 days ago by 500 level student:</span>
-            <br />
-            I'm looking for 2 rooms any area close to school. I will pay
-            immediately.
+          <div class="space-y-2 border border-gray-300 bg-gray-50 p-3 rounded-lg hover:bg-purple-100 cursor-pointer">
+            <div class="flex space-x-2 text-xs text-gray-600 lowercase">
+              <span class="bg-gray-200 border border-gray-300 py-0.5 px-1 rounded-full">
+                29 Days Ago
+              </span>
+              <span class="bg-gray-200 border border-gray-300 py-0.5 px-1 rounded-full">
+                400 level student
+              </span>
+            </div>
+            <div>
+              I'm looking for 2 rooms any area close to school. I will pay
+              immediately.
+            </div>
+          </div>
+          <div class="space-y-2 border border-gray-300 bg-gray-50 p-3 rounded-lg hover:bg-purple-100 cursor-pointer">
+            <div class="flex space-x-2 text-xs text-gray-600 lowercase">
+              <span class="bg-gray-200 border border-gray-300 py-0.5 px-1 rounded-full">
+                30 Days Ago
+              </span>
+              <span class="bg-gray-200 border border-gray-300 py-0.5 px-1 rounded-full">
+                400 level student
+              </span>
+            </div>
+            <div>
+              I and my friends are looking for 2 bedroom flat. Should be at most
+              1 drop from main campus. Very URGENT pls.
+            </div>
           </div>
         </div>
       </div>
