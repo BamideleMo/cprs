2850881d07be2ff4297278fec92e5e1a766cd03c
bamidelemo <bamidelemo@gmail.com>
2025-03-15 10:02:58 +0100

    .

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -176,7 +176,7 @@ function Requests() {
           >
             <div class="fixed z-40 bg-slate-900 bg-opacity-90 flex items-center h-screen w-screen top-0 bottom-0 left-0 right-0">
               <div class="w-11/12 sm:w-[620px] mx-auto">
-                <div class="w-28 h-28 rounded-full bg-white mx-auto animate-pulse flex items-center">
+                <div class="w-28 h-28 rounded-full bg-white mx-auto animate-bounce flex items-center">
                   <span class="w-fit mx-auto">
                     <Logo />
                   </span>
