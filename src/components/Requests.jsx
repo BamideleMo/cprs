fe7c6178d17b45b5498cd73eb45d6bf64fc8e224
bamidelemo <bamidelemo@gmail.com>
2025-03-16 12:46:42 +0100

    .

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -174,15 +174,17 @@ function Requests() {
               </>
             }
           >
-            <div class="fixed z-40 bg-slate-900 bg-opacity-90 flex items-center h-screen w-screen top-0 bottom-0 left-0 right-0">
-              <div class="w-11/12 sm:w-[620px] mx-auto">
-                <div class="w-28 h-28 rounded-full bg-white mx-auto animate-bounce flex items-center">
-                  <span class="w-fit mx-auto">
-                    <Logo />
-                  </span>
+            <Show when={JSON.parse(localStorage.getItem("OffKUni"))}>
+              <div class="fixed z-40 bg-slate-900 bg-opacity-90 flex items-center h-screen w-screen top-0 bottom-0 left-0 right-0">
+                <div class="w-11/12 sm:w-[620px] mx-auto">
+                  <div class="w-28 h-28 rounded-full bg-white mx-auto animate-bounce flex items-center">
+                    <span class="w-fit mx-auto">
+                      <Logo />
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Show>
           </Show>
         </div>
       </div>
