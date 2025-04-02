a4dfd1ef1ef0f440bfdf1825fef042b831362ed3
bamidelemo <bamidelemo@gmail.com>
2025-04-02 10:57:39 +0100

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,7 +46,7 @@ function Footer(props) {
         <div class="mt-20">&nbsp;</div>
         <div class="bg-black text-gray-200 border-t px-2 flex items-center bottom-0 left-0 right-0 fixed h-20 overflow-hidden">
           <div class="w-full px-1 sm:w-[620px] mx-auto">
-            <div class=" text-yellow-400">Advertisement:</div>
+            <div class=" text-yellow-400">Ad:</div>
             <div>
               Sweet gist by students & for students of UNIABUJA every Saturday
               on:{" "}
