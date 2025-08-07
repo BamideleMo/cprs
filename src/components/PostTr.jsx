d416d6a721a9f821577ecfe5d61598b96e7bae2b
bamidelemo <bamidelemo@gmail.com>
2025-08-07 06:46:04 +0100

    .

--- src/components/PostTr.jsx
+++ src/components/PostTr.jsx
@@ -106,21 +106,21 @@ function PostTr(props) {
           <div class="flex flex-col md:flex-row md:justify-between">
             <div>
               <div class="flex space-x-1 md:space-x-2">
-                <div class="text-[11px] text-gray-600">
-                  {props.rsc.days === 0 ? (
-                    <span class="capitalize">Today</span>
-                  ) : props.rsc.days <= 1 ? (
-                    props.rsc.days + " Day Ago"
-                  ) : (
-                    props.rsc.days + " Days Ago"
-                  )}
-                </div>
                 <span class="block mt-0 space-x-1 md:space-x-2 text-[11px]">
+                  <span class="bg-white text-gray-600 border border-gray-800 p-0.5 rounded">
+                    {props.rsc.days === 0 ? (
+                      <span class="capitalize">Today</span>
+                    ) : props.rsc.days <= 1 ? (
+                      props.rsc.days + " Day Ago"
+                    ) : (
+                      props.rsc.days + " Days Ago"
+                    )}
+                  </span>
                   <span class="bg-orange-300 border border-orange-600 p-0.5 rounded">
                     {props.rsc.category}
                   </span>
                   <span class=" bg-gray-300 border border-gray-600 p-0.5 rounded">
-                    <b>ID:</b> OffK{props.rsc.id}
+                    <b>ID:</b> K{props.rsc.id}
                   </span>
                 </span>
                 <span class="w-fit">
