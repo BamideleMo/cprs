25f9bc625db21be715a8b2a2664795619b14b801
BamideleMO <bamidelemo@gmail.com>
2026-01-01 09:54:42 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import chatIcon from "./assets/whatsapp-icon.png";
 import welcomePicture from "./assets/CPRS-graduation-picture-2025.jpg";
 import TextCarousel from "./components/TextCarousel";
 import ArrRight from "./components/icons/ArrRight";
+import Files from "./components/icons/Files";
 
 function App() {
   return (
@@ -113,7 +114,7 @@ function App() {
             <div class="flex-1">
               <img src={welcomePicture} class="w-full rounded-lg" />
             </div>
-            <div class="flex-1">
+            <div class="flex-1 drop-shadow-lg">
               <h1 class="mt-6 uppercase text-base [word-spacing:5px] text-red-700 font-bold">
                 Welcome to CPRS
               </h1>
@@ -142,7 +143,147 @@ function App() {
         </div>
         <div class="border-t border-gray-300 mt-20">
           <div class="w-11/12 lg:w-[1200px] text-sm mx-auto text-gray-500 my-20">
-            xx
+            <h1 class="mt-6 uppercase text-base [word-spacing:5px] text-red-700 font-bold">
+              Our Faculty
+            </h1>
+            <h2 class="mt-6 text-2xl font-bold text-black">CPRS Programmes</h2>
+            <div class="mt-2 text-xl w-3/5 font-bold">
+              Our curriculum empowers students with the critical mediation and
+              leadership skills necessary to navigate the complexities of
+              today’s global conflicts.
+            </div>
+            <div class="mt-12 grid grid-cols-1 md:grid-cols-4 gap-6">
+              <div class="border border-gray-200 p-4 drop-shadow-lg">
+                <h2 class="text-lg text-blue-800 font-bold leading-tight">
+                  DIPLOMA PROGRAMMES
+                </h2>
+                <div class="mt-2 text-sm flex space-x-1">
+                  <Files />
+                  <b>3 Programmes</b>
+                </div>
+                <ul class="my-4 space-y-3">
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Diploma in Peace and Conflict Studies
+                  </li>
+
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Diploma in Peace and Counselling Studies
+                  </li>
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Diploma in Security Management Technology
+                  </li>
+                </ul>
+                <div class="pt-4">
+                  <A
+                    href="/"
+                    class="flex text-green-700 font-bold hover:opacity-60"
+                  >
+                    <ArrRight />
+                    <span class="-mt-0.5">Entry Requirements</span>
+                  </A>
+                </div>
+              </div>
+              <div class="border border-gray-200 p-4 drop-shadow-lg">
+                <h2 class="text-lg text-blue-800 font-bold leading-tight">
+                  UNDERGRADUATE PROGRAMMES
+                </h2>
+                <div class="mt-2 text-sm flex space-x-1">
+                  <Files />
+                  <b>4 Programmes</b>
+                </div>
+                <ul class="my-4 space-y-3">
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Bachelor of Science Peace & Conflict Studies
+                  </li>
+
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Bachelor of Science, Criminology and Security Technology
+                  </li>
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Bachelor of Arts, Peace and Counselling Studies
+                  </li>
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Bachelor of Arts Education & Conflict Studies
+                  </li>
+                </ul>
+                <div class="pt-4">
+                  <A
+                    href="/"
+                    class="flex text-green-700 font-bold hover:opacity-60"
+                  >
+                    <ArrRight />
+                    <span class="-mt-0.5">Entry Requirements</span>
+                  </A>
+                </div>
+              </div>
+              <div class="border border-gray-200 p-4 drop-shadow-lg">
+                <h2 class="text-lg text-blue-800 font-bold leading-tight">
+                  POSTGRADUATE DIPLOMA
+                </h2>
+                <div class="mt-2 text-sm flex space-x-1">
+                  <Files />
+                  <b>2 Programmes</b>
+                </div>
+                <ul class="my-4 space-y-3">
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Postgraduate Diploma Conflict Transformation
+                  </li>
+
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Postgraduate Diploma Criminology & Security Technology
+                  </li>
+                </ul>
+                <div class="pt-4">
+                  <A
+                    href="/"
+                    class="flex text-green-700 font-bold hover:opacity-60"
+                  >
+                    <ArrRight />
+                    <span class="-mt-0.5">Entry Requirements</span>
+                  </A>
+                </div>
+              </div>
+              <div class="border border-gray-200 p-4 drop-shadow-lg">
+                <h2 class="text-lg text-blue-800 font-bold leading-tight">
+                  MASTERS PROGRAMMES
+                </h2>
+                <div class="mt-2 text-sm flex space-x-1">
+                  <Files />
+                  <b>5 Programmes</b>
+                </div>
+                <ul class="my-4 space-y-3">
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Master of Arts Conflict Transformation
+                  </li>
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Master of Science Criminology & Security Studies
+                  </li>
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Master of Arts Peace & Public Policy
+                  </li>
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Master of Arts Theology & Conflict Studies
+                  </li>
+                  <li class="border-l-4 border-red-700 pl-1">
+                    Master of Arts Education & Conflict Studies
+                  </li>
+                </ul>
+                <div class="pt-4">
+                  <A
+                    href="/"
+                    class="flex text-green-700 font-bold hover:opacity-60"
+                  >
+                    <ArrRight />
+                    <span class="-mt-0.5">Entry Requirements</span>
+                  </A>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bg-blue-700 py-20 text-white">
+          <div class="w-11/12 lg:w-[1200px] text-sm mx-auto">
+            <h2 class="mt-6 text-2xl font-bold text-blue-200">CENTRE FOR PROFESSIONAL DEVELOPMENT (CPD)</h2>
           </div>
         </div>
       </div>
