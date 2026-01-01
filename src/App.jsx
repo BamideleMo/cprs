a03281e113b2be995ed6acbc788280ead4f7772b
BamideleMO <bamidelemo@gmail.com>
2026-01-01 07:28:31 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { MetaProvider, Title, Meta, Link } from "@solidjs/meta";
 import { A } from "@solidjs/router";
 import logo from "../CPRS-logo.jpg";
 import chatIcon from "./assets/whatsapp-icon.png";
+import welcomePicture from "./assets/CPRS-graduation-picture-2025.jpg";
 import TextCarousel from "./components/TextCarousel";
+import ArrRight from "./components/icons/ArrRight";
 
 function App() {
   return (
@@ -18,12 +20,12 @@ function App() {
                 <div class="w-20">
                   <img src={logo} class="w-full" />
                 </div>
-                <div class="uppercase -space-y-2 pt-3 font-bold text-lg">
+                <div class="drop-shadow-lg uppercase -space-y-2 pt-3 font-bold text-lg">
                   <div>College of Peace</div>
                   <div>and Reconciliation Studies, Jos</div>
                 </div>
               </div>
-              <div class="w-fit">
+              <div class="w-fit drop-shadow-lg">
                 <ul class="text-sm mt-6 flex space-x-12">
                   <li>
                     <A
@@ -70,7 +72,7 @@ function App() {
             </div>
           </div>
         </header>
-        <div class="bg-green-700 py-4 text-center text-white text-sm">
+        <div class="border-t border-yellow-200 bg-green-700 py-2 text-center text-white text-sm">
           <div class="w-11/12 lg:w-[1200px] mx-auto">
             Sales of Admission forms is ongoing.{" "}
             <A href="/" class="underline hover:opacity-60">
@@ -94,7 +96,7 @@ function App() {
                 </A>
                 <A
                   href="/"
-                  class="flex w-fit space-x-1 text-red-700 bg-gray-50 p-3 border border-black hover:opacity-60"
+                  class="flex w-fit space-x-1 text-red-700 bg-gray-50 p-3 border border-red-700 hover:opacity-60"
                 >
                   <span>
                     <img src={chatIcon} class="w-4 mt-0.5" />
@@ -106,8 +108,42 @@ function App() {
           </div>
           <div class="flex-1 bg-image">&nbsp;</div>
         </div>
-        <div class="w-11/12 lg:w-2/4 text-lg mx-auto text-gray-400 text-center">
-          xxx
+        <div class="w-11/12 lg:w-[1200px] text-sm mx-auto text-gray-500 my-20">
+          <div class="flex space-x-28 drop-shadow-xl">
+            <div class="flex-1">
+              <img src={welcomePicture} class="w-full rounded-lg" />
+            </div>
+            <div class="flex-1">
+              <h1 class="mt-6 uppercase text-base [word-spacing:5px] text-red-700 font-bold">
+                Welcome to CPRS
+              </h1>
+              <h2 class="my-6 text-2xl font-bold text-black">
+                Join the College of Peace and Reconciliation Studies
+              </h2>
+              <h2 class="text-lg">
+                We have dedicated and experienced faculty members with national
+                and international experiences that are committed to
+                professionalism and integrity. You will find in them a rich
+                contextual experience in peacebuilding.
+              </h2>
+              <div class="mt-10 text-green-700">
+                <A
+                  href="#"
+                  class="w-fit font-bold text-base flex space-x-1 hover:opacity-60"
+                >
+                  <span class="-mt-0.5">
+                    <ArrRight />
+                  </span>
+                  <span class="">About CPRS</span>
+                </A>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="border-t border-gray-300 mt-20">
+          <div class="w-11/12 lg:w-[1200px] text-sm mx-auto text-gray-500 my-20">
+            xx
+          </div>
         </div>
       </div>
     </MetaProvider>
